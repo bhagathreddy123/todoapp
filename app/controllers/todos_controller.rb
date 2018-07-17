269ffbe913cbe508493585f1d06b1c63aa5a0838
bhagath <katta_reddy04@yahoo.com>
2018-07-17 06:51:44 +0000

    refactor the code using partials and flash message display

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
- 
+ before_action :set_todo, only: [:edit,:update,:show,:destroy]
    def index
        @todos = Todo.all
    end
@@ -7,7 +7,7 @@ class TodosController < ApplicationController
         @todo = Todo.new
     end
     def create
-      @todo = Todo.new(todo_params)
+        @todo = Todo.new(todo_params)
         if @todo.save
            flash[:notice] = "Todo successfully created"
             redirect_to todos_path
@@ -18,15 +18,26 @@ class TodosController < ApplicationController
     def edit
     end
     def update
+        if @todo.update(todo_params)
+            flash[:notice] = "edited todo successfully"
+            redirect_to todos_path
+        else
+            render :edit
+        end
     end
     def show
     end
     def destroy
+        if @todo.destroy
+            flash[:notice] = "todo deleted successfully"
+            redirect_to todos_path
+        end
     end
     
     private 
     
     def set_todo
+         @todo = Todo.find(params[:id])
     end
     def todo_params
         params.require(:todo).permit!
