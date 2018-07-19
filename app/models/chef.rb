32c43e1c205d7c74e0011549c0475307dbb4ad4c
bhagath <katta_reddy04@yahoo.com>
2018-07-19 10:50:20 +0000

    chef email validation with test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,9 @@
 class Chef < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :recipes
-    validates :email, :presence => :true
-    validates :chefname, :presence => :true
+    validates :chefname, presence: true, length: { maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
