77bd3f55d4bd445d38822e5f30bcd1c61fc5507f
Bhagath <bhagath@sproutssolutions.com>
2018-08-06 10:40:30 +0530

    add ingredient model and many to many associations with recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,7 @@ class Recipe < ApplicationRecord
     validates :name, :presence => :true
     validates :description, presence: true, length: { minimum: 5, maximum: 500 }
     validates :chef_id, :presence => :true
+    has_many :recipe_ingredients,dependent: :destroy
+    has_many :ingredients, through: :recipe_ingredients
     default_scope -> { order(updated_at: :desc) }
 end
